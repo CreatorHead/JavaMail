b48e313f1616c33a2aee0754304bec10eaf81c00
creatorHead <droidbit1@gmail.com>
2018-03-13 23:06:14 +0530

    Java Mail Commit (1st)

---
+++ src/main/java/com/mail/JavaMailClient.java
@@ -0,0 +1,47 @@
+package com.mail;
+
+import java.util.Properties;
+
+import javax.mail.Message;
+import javax.mail.PasswordAuthentication;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
+
+public class JavaMailClient {
+	
+	public boolean sendMessage(String username, String password, String to,String sub, String msg){
+		boolean state = false;
+		Properties props = new Properties();
+		props.put("mail.smtp.auth", "true");
+		props.put("mail.smtp.starttls.enable", "true");
+		props.put("mail.smtp.host", "smtp.gmail.com");
+		props.put("mail.smtp.port", "587");
+		
+		Session session = Session.getInstance(props,
+				new javax.mail.Authenticator(){
+			protected PasswordAuthentication getPasswordAuthentication(){
+				return new PasswordAuthentication(username, password);
+			}
+		});
+		System.out.println(session.getClass());
+		
+		try{
+			
+			Message message = new MimeMessage(session);
+			message.setFrom(new InternetAddress(username));
+			message.setRecipients(Message.RecipientType.TO, InternetAddress.parse(to));
+			message.setSubject(sub);
+			message.setText(msg);
+			Transport.send(message);
+			System.out.println("Message was Sent");
+			state = true;
+			
+		}catch(Exception e){
+			e.printStackTrace();
+		}
+		return state;
+	}
+
+}
